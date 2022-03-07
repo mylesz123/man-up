a856c13e1f818088a35693adc3585a16460c5321
Myles Young <myles.young@mailchimp.com>
2022-03-07 13:34:15 -0700

    modify css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       colors: {
         green: 'var(--green)',
       },
+      screens: {
+        sm: { max: '767px' },
+        md: { min: '768px', max: '1024px' },
+        'md-down': { max: '1024px' },
+        // large org value: 1279
+        lg: { min: '1025px' },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
